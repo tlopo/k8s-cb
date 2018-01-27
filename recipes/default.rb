3fa9f4b160b570757fbbef935c561c600edf910c
Tiago Lopo Da Silva <tlopo@ibm.com>
2018-01-27 14:06:49 +0100

    Creat a x509 certificate

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,10 +4,44 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-docker_service 'default'  do 
-  action [:create, :start]
+hostname = node['hostname']
+ip = node['ipaddress']
+cert_dir = node['cert_dir']
+ca_cert = Base64.decode64 node['ca-cert']
+ca_key = Base64.decode64 node['ca-key']
+
+
+key_usage = 'nonRepudiation, digitalSignature, keyEncipherment'
+basic_constraints = 'CA:FALSE'
+subject = "/CN=#{hostname}"
+min_validity = 30 * 24 * 60 * 60 # 30 days
+subject_alt_name = "IP:127.0.0.1,IP:#{ip},DNS:localhost,DNS:#{hostname}"
+
+
+directory "#{cert_dir}" do 
+  recursive true
+  owner user
+  group user
+  mode '0755'
 end
 
-file '/etc/k8s' do
-  content 'Kubernetes cookbook run'
+file "#{cert_dir}/ca-cert.pem" do 
+  content ca_cert
+  owner user
+  group user
+  mode '0644'
 end
+
+x509_certificate 'node' do
+  action [:create, :update]
+  ca_cert ca_cert
+  ca_key  ca_key
+  cert_dst "#{cert_dir}/#{hostname}-cert.pem"
+  key_dst "#{cert_dir}/#{hostname}-key.pem"
+  subject subject
+  basic_constraints basic_constraints
+  key_usage key_usage
+  subject_alt_name subject_alt_name
+  min_validity min_validity
+end
+
