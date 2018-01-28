1acc469192af2655b437a9882e49aba0155be8ce
Tiago Lopo Da Silva <tlopo@ibm.com>
2018-01-28 13:42:55 +0100

    Working on kubemaster recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -29,6 +29,12 @@ etcd_servers = node['etcd']['servers'].values.map{|v| "#{etcd_scheme}://#{v}:237
 image = 'gcr.io/google_containers/hyperkube-amd64:v1.9.2'
 master_vip = node['kubernetes']['master_vip'] 
 cluster_service_ip_range = node['kubernetes']['cluster_service_ip_range']
+network_driver = node['network']['driver']
+cluster_domain = node['kubernetes']['cluster_domain']
+cluster_dns = node['kubernetes']['cluster_dns']
+ha_minions = node['kubernetes']['minion-ha']
+is_ha_minion = false
+ha_minions.each_key {|k| is_ha_minion = true if ha_minions[k] == ip}
 
 
 directory k8s_binary_dir do
@@ -63,4 +69,52 @@ template "#{kubeconfig}" do
   mode '0644'
 end
 
+execute 'disable_swap' do
+  command 'swapoff -a'
+  only_if 'swapon -s | grep -qPo "\d+"'
+end
+
+execute 'remove_swap_from_fstab' do
+  command 'sed -ri "/swap/d" /etc/fstab'
+  only_if 'grep swap /etc/fstab'
+end
+
+systemd_unit 'kubelet.service'  do
+  cmd = [
+    '/opt/kubernetes/bin/kubelet',
+    '--address=0.0.0.0',
+    "--kubeconfig=#{kubeconfig}",
+    "--cluster-domain=#{cluster_domain}",
+    "--cluster-dns=#{cluster_dns}"
+  ]
+
+  cmd << '--node-labels=ha-minion=true' if is_ha_minion
+
+  if net_driver == 'calico'
+    cmd << '--network-plugin=cni'
+    cmd << '--cni-conf-dir=/etc/cni/net.d'
+    cmd << '--cni-bin-dir=/opt/cni/bin'
+  end
+
+  content <<-EOF.gsub(/^  /,'')
+  [Unit]
+  Description=Kubelet
+  After=docker.service
+  Requires=docker.service
+   
+  [Service]
+  TimeoutStartSec=0
+  Restart=always
+  ExecStart=#{cmd.join(' ')}
+  ExecStop=/bin/kill $MAINPID
+  Restart=always
+  RestartSec=10s
+  NotifyAccess=all
+  
+  [Install]
+  WantedBy=multi-user.target
+  EOF
+  notifies :restart, 'service[kubelet]', :delayed
+  action [:create, :enable]
+end
 
