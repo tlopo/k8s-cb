2efdd81303e8fb03706f6067865eee0be67ef22b
Tiago Lopo Da Silva <tlopo@ibm.com>
2018-01-28 17:32:56 +0100

    Refactoring

--- recipes/default.rb
+++ recipes/default.rb
@@ -18,6 +18,18 @@ ip = node['ipaddress']
 hostname = node['hostname']
 cert_dir = node['cert_dir']
 
+is_etcd = false
+is_master = false
+is_minion = false
+
+etcd_servers = node['etcd']['servers']
+masters = node['kubernetes']['master']
+minions = node['kubernetes']['minion']
+
+etcd_servers.keys.each {|k| is_etcd = true if etcd_servers[k] == ip}
+masters.keys.each {|k| is_master = true if masters[k] == ip}
+minions.keys.each {|k| is_minion = true if minions[k] == ip}
+
 k8s_node_binaries_url = 'https://dl.k8s.io/v1.9.2/kubernetes-node-linux-amd64.tar.gz'
 k8s_binary_dir = '/opt/kubernetes/bin'
 kubeconfig = '/opt/kubernetes/kubeconfig'
