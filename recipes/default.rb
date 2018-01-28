0660b7bee7f6217767eda50667c0ebe0c4bfc77a
Tiago Lopo Da Silva <tlopo@ibm.com>
2018-01-28 18:02:49 +0100

    Refactoring

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,6 +2,28 @@
 # Recipe:: default
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
+
+ip = node['ipaddress']
+hostname = node['hostname']
+cert_dir = node['cert_dir']
+
+is_etcd = false
+is_master = false
+is_minion = false
+
+etcd_servers = node['etcd']['servers']
+masters = node['kubernetes']['master']
+minions = node['kubernetes']['minion']
+
+#etcd_servers.keys.each {|k| is_etcd = true if etcd_servers[k] == ip}
+#masters.keys.each {|k| is_master = true if masters[k] == ip}
+#minions.keys.each {|k| is_minion = true if minions[k] == ip}
+
+unless is_etcd or is_master or is_minion
+  Chef::Log.warn "#{hostname} does not belong to none of this groups [minion, master, etcd]"
+  return 
+end
+
 yum_package 'docker'
 
 service 'docker'  do 
@@ -11,8 +33,5 @@ end
 service 'docker' do
   action :enable
 end
-
-
-Chef::Log.info "DIR => #{__dir__}"
 
 include_recipe "#{cookbook_name}::x509-certs"
