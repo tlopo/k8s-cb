cc8216ac94c0c48390b29e8e0a6fdfe6f29f2516
Tiago Lopo Da Silva <tlopo@ibm.com>
2018-01-30 11:06:04 +0100

    Working on add-on recipe

--- recipes/add-on.rb
+++ recipes/add-on.rb
@@ -43,6 +43,7 @@ ruby_block 'Create add-ons' do
       cmd= [ "kubectl --namespace kube-system get #{templates[template]} ||",
              "kubectl replace --force -f #{k8s_manifest_dir}/#{template} --validate=false"].join(' ')
       STDERR.write "#{cmd}\n" 
+      Chef::Log.warn "#{cmd}\n" 
       out = `#{cmd}`
       Chef::Log.error out unless $?.success?
       raise "#{template} failed" unless $?.success?
